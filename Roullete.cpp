f7c358341f0ec3c914beed34712a0a6f13c40e7e
Spyro <epicdudeness677@gmail.com>
2021-10-06 14:26:44 +0200

    Implementd Split bet.

--- Roullete.cpp
+++ Roullete.cpp
@@ -12,10 +12,14 @@ namespace Roulette
 
 
 	std::array<std::string, 20> userInstructions = {
-
+		"The rules are as follows!\n",
+		"You begin by deciding how you are going to bet into roulette.\n",
+		"This decision will determine how roulette is going to be played.\n",
+		"Once you have come to a choice, a board is going to appear and we will instruct you what to do next!\n",
 	};
 
-	std::array<std::array < int, 3>, 12> roulleteBoard = { {
+	const std::array<std::array < int, 3>, 13> rouletteBoard = { {
+		{ 0, 0, 0},
 		{ 1, 2, 3},
 		{ 4, 5, 6},
 		{ 7, 8, 9},
@@ -32,12 +36,46 @@ namespace Roulette
 	}
 	};
 
+	std::array<std::array < SelectionType, 3>, 13> rouletteSelectionBoard = { {
+		{SelectionType::None, SelectionType::None, SelectionType::None},
+		{SelectionType::None, SelectionType::None, SelectionType::None},
+		{SelectionType::None, SelectionType::None, SelectionType::None},
+		{SelectionType::None, SelectionType::None, SelectionType::None},
+		{SelectionType::None, SelectionType::None, SelectionType::None},
+		{SelectionType::None, SelectionType::None, SelectionType::None},
+		{SelectionType::None, SelectionType::None, SelectionType::None},
+		{SelectionType::None, SelectionType::None, SelectionType::None},
+		{SelectionType::None, SelectionType::None, SelectionType::None},
+		{SelectionType::None, SelectionType::None, SelectionType::None},
+		{SelectionType::None, SelectionType::None, SelectionType::None},
+		{SelectionType::None, SelectionType::None, SelectionType::None},
+		{SelectionType::None, SelectionType::None, SelectionType::None}
+
+   }
+	};
+
 	BetType currentBetType;
 
 
 	void PlayGame()
 	{
+		rouletteSelectionBoard = { {
+		{SelectionType::None, SelectionType::None, SelectionType::None},
+		{SelectionType::None, SelectionType::None, SelectionType::None},
+		{SelectionType::None, SelectionType::None, SelectionType::None},
+		{SelectionType::None, SelectionType::None, SelectionType::None},
+		{SelectionType::None, SelectionType::None, SelectionType::None},
+		{SelectionType::None, SelectionType::None, SelectionType::None},
+		{SelectionType::None, SelectionType::None, SelectionType::None},
+		{SelectionType::None, SelectionType::None, SelectionType::None},
+		{SelectionType::None, SelectionType::None, SelectionType::None},
+		{SelectionType::None, SelectionType::None, SelectionType::None},
+		{SelectionType::None, SelectionType::None, SelectionType::None},
+		{SelectionType::None, SelectionType::None, SelectionType::None},
+		{SelectionType::None, SelectionType::None, SelectionType::None}
 
+   }
+		};
 		system("cls");
 
 		User::currentBetAmm = 0;
@@ -77,14 +115,26 @@ namespace Roulette
 
 		}
 
+		
+		std::cin.ignore(10000, '\n');
 		game::currentGameState = User::hasUserWon ? game::GameState::Won : game::GameState::Lost;
+	}
+
+	bool TryGuessACorner()
+	{
+		system("cls");
+		std::cout << "Select one of four numbers that are adjacent to each other between " << rouletteStartingNumber << " and " << rouletteNumberAmm << ".\nFirst Number:";
+		return true;
 	}
 
 	bool TryGuessASplit()
 	{
+
 		system("cls");
+		DrawBoard();
 		std::cout << "Select two adjacent numbers between " << rouletteStartingNumber << " and " << rouletteNumberAmm << ".\nFirst Number:";
-		int firstNumber = User::GetConstrainedNumericalUserInput(0, rouletteNumberAmm);
+		int firstNumber = Search(rouletteBoard, User::GetConstrainedNumericalUserInput(0, rouletteNumberAmm));
+
 
 		if (firstNumber == User::invalidNumericalInput)
 		{
@@ -92,37 +142,292 @@ namespace Roulette
 			system("pause");
 			return TryGuessASplit();
 		}
+
+		SelectElementFromBoard(firstNumber, SelectionType::User);
+		std::array<std::array<int, 2>, 4> foundLegalPositions = GetLegalPositions(firstNumber);
+
+		for (int y = 0; y < foundLegalPositions.size(); y++)
+		{
+			if (IsPositionOutOfBounds(foundLegalPositions[y])) continue;
+			rouletteSelectionBoard[foundLegalPositions[y][0]][foundLegalPositions[y][1]] = SelectionType::LegalOption;
+
+		}
+
+
+		system("cls");
+		DrawBoard();
+		std::cout << "Select two adjacent numbers between " << rouletteStartingNumber << " and " << rouletteNumberAmm << ".\n";
 		std::cout << "Second Number:";
-		int secondNumber = GetValidatedNumberThatIsAdjacent(firstNumber, User::GetConstrainedNumericalUserInput(0, rouletteNumberAmm));
+
+
+
+		int secondNumber = GetValidatedNumberThatIsAdjacent(foundLegalPositions, Search(rouletteBoard, User::GetConstrainedNumericalUserInput(rouletteStartingNumber, rouletteNumberAmm)));
 	
+
+
+		if (secondNumber == User::invalidNumericalInput)
+		{
+			std::cout << game::errorMessage << std::endl;
+			system("pause");
+			return TryGuessASplit();
+		}
+		SelectElementFromBoard(secondNumber, SelectionType::User);
+		int randomNumber = GetRandomNumberFromRoulette();
+
+		SelectElementFromBoard(randomNumber, SelectionType::Game);
+		system("cls");
+
+		DrawBoard();
+		std::cout << "RNGJesus dictated that " << randomNumber << " will be selected!";
+
+		return randomNumber == firstNumber || randomNumber == secondNumber;
+
 	}
 
-	int GetValidatedNumberThatIsAdjacent(int anOriginPoint, int aNewNumber)
+	int GetValueFromPosition(std::array<int, 2> aPosition)
 	{
-		
+		if (aPosition[0] == User::invalidNumericalInput || aPosition[1] == User::invalidNumericalInput) return User::invalidNumericalInput;
+
+		return rouletteBoard[aPosition[0]][aPosition[1]];
+	}
+
+
+	std::array<std::array<int, 2>, 4> GetLegalPositions(int anOriginPoint)
+	{
+		std::array<int, 2> anOriginPointGridPos = GetIndex(rouletteBoard, anOriginPoint);
+		return GetLegalNumbers(anOriginPointGridPos);
+	}
+
+	int GetValidatedNumberThatIsAdjacent(std::array<std::array<int, 2>, 4> someLegalPositions, int aNewNumber)
+	{
+
 		if (aNewNumber == User::invalidNumericalInput) return User::invalidNumericalInput;
 
 
+		std::array<int, 2> aNewNumberGridPos = GetIndex(rouletteBoard, aNewNumber);
 
+		for (int legalIndex = 0; legalIndex < someLegalPositions.size(); legalIndex++)
+		{
+			if (someLegalPositions[legalIndex][0] == aNewNumberGridPos[0] &&
+				someLegalPositions[legalIndex][1] == aNewNumberGridPos[1])
+			{
+				return aNewNumber;
+			}
+		}
 
+		return User::invalidNumericalInput;
 
 	}
 
-
-	int Find(std::array<std::array<int, 3>, 12> aBoard, int aNumberToCompare, int &aFoundColumnIndex, int &aFoundRowIndex)
+	int Search(std::array<std::array<int, 3>, 13> aBoard, int anElementToFind)
 	{
-		for (int i = 0; i < aBoard.size(); i++)
+		for (int y = 0; y < aBoard.size(); y++)
 		{
-			for (int x = 0; x < aBoard[i].size(); x++)
+			for (int x = 0; x < aBoard[y].size(); x++)
 			{
-				if (aBoard[i][x] == aNumberToCompare)
+				if (aBoard[y][x] == anElementToFind)
 				{
-					aFoundColumnIndex = i;
-					aFoundRowIndex = x;
-					return aBoard[i][x];
+					return aBoard[y][x];
 				}
 			}
 		}
+		return User::invalidNumericalInput;
+	}
+
+	std::array<int, 2> GetIndex(std::array<std::array<int, 3>, 13> aBoard, int anElementToFindIndex)
+	{
+		std::array<int, 2> result = { User::invalidNumericalInput, User::invalidNumericalInput };
+		for (int y = 0; y < aBoard.size(); y++)
+		{
+			for (int x = 0; x < aBoard[y].size(); x++)
+			{
+				if (aBoard[y][x] == anElementToFindIndex)
+				{
+					result[0] = y;
+					result[1] = x;
+					return result;
+				}
+			}
+		}
+		return result;
+	}
+
+
+	std::array < std::array <int, 2>, 4 > GetLegalNumbers(std::array<int, 2> aGridCoordinate)
+	{
+		std::array< std::array <int, 2>, 4> result = { {
+					{User::invalidNumericalInput, User::invalidNumericalInput },
+					{User::invalidNumericalInput, User::invalidNumericalInput},
+					{User::invalidNumericalInput, User::invalidNumericalInput},
+					{User::invalidNumericalInput, User::invalidNumericalInput} }
+		};
+		switch (currentBetType)
+		{
+			case Roulette::BetType::Split:
+				{
+
+
+					int upperYIndex = aGridCoordinate[0] + 1;
+					int lowerYIndex = aGridCoordinate[0] - 1;
+
+					int upperXIndex = aGridCoordinate[1] + 1;
+					int lowerXIndex = aGridCoordinate[1] - 1;
+
+					if (upperYIndex < rouletteBoard.size())
+					{
+						result[0] = { upperYIndex, aGridCoordinate[1] };
+					}
+
+					if (lowerYIndex >= 0)
+					{
+						result[1] = { lowerYIndex, aGridCoordinate[1] };
+					}
+
+					if (upperXIndex < rouletteBoard[aGridCoordinate[0]].size())
+					{
+						result[2] = { aGridCoordinate[0], upperXIndex };
+
+					}
+					if (lowerXIndex >= 0)
+					{
+						result[3] = { aGridCoordinate[0], lowerXIndex };
+					}
+
+					break;
+				}
+
+			case Roulette::BetType::Corner:
+				break;
+			case Roulette::BetType::OddOrEven:
+				break;
+			case Roulette::BetType::Column:
+				break;
+			case Roulette::BetType::RedOrBlack:
+				break;
+		}
+
+		return result;
+
+	}
+
+	void DrawBoard()
+	{
+		const int singleDigidElements = 4;
+		const std::string userSelectionGraphic = "#";
+		const std::string gameSelectionGraphic = "X";
+
+		const std::string legalOptionSelectionGraphicL = "<";
+		const std::string legalOptionSelectionGraphicR = ">";
+		const std::string legalOptionSelectionGraphicMiddle = "I";
+
+		switch (rouletteSelectionBoard[0][0])
+		{
+			case SelectionType::User:
+				{
+					std::cout << std::string("   [" + userSelectionGraphic + "]");
+					break;
+				}
+			case SelectionType::Game:
+				{
+					std::cout << std::string("   [" + gameSelectionGraphic + "]");
+					break;
+				}
+			case SelectionType::LegalOption:
+				{
+					std::cout << std::string("   " + legalOptionSelectionGraphicL + gameSelectionGraphic + legalOptionSelectionGraphicR);
+					break;
+				}
+			case SelectionType::None:
+				{
+					std::cout << "   [0]";
+					break;
+				}
+		}
+		std::cout << std::endl;
+
+
+		for (int y = 0; y < rouletteBoard.size(); y++)
+		{
+			if (y == 0) continue;
+			for (int x = 0; x < rouletteBoard[y].size(); x++)
+			{
+
+				if (x == 0)
+				{
+					std::cout << (rouletteSelectionBoard[y][x] == SelectionType::LegalOption ? (y < singleDigidElements ? " " + legalOptionSelectionGraphicL : legalOptionSelectionGraphicL) : (y < singleDigidElements ? " [" : "["));
+				}
+				switch (rouletteSelectionBoard[y][x])
+				{
+					case SelectionType::User:
+						{
+							std::cout << userSelectionGraphic;
+							break;
+						}
+					case SelectionType::Game:
+						{
+							std::cout << (y < singleDigidElements ? gameSelectionGraphic : " " + gameSelectionGraphic);
+							break;
+						}
+					case SelectionType::None:
+					case SelectionType::LegalOption:
+						{
+							std::cout << std::to_string(rouletteBoard[y][x]);
+							break;
+						}
+				}
+
+				if (x == rouletteBoard[y].size() - 1)
+				{
+
+					std::cout << (rouletteSelectionBoard[y][x] == SelectionType::LegalOption ? legalOptionSelectionGraphicR : "]");
+				}
+				else
+				{
+
+					const int xNext = x + 1;
+					std::cout << (rouletteSelectionBoard[y][x] == SelectionType::LegalOption || (x == 0 && rouletteSelectionBoard[y][xNext] == SelectionType::LegalOption) ? legalOptionSelectionGraphicMiddle : "|");
+
+
+
+
+				}
+			}
+			std::cout << std::endl;
+		}
+	}
+
+	void SelectElementFromBoard(int aNumberToSelect, SelectionType aSelectionOwner)
+	{
+		if (aNumberToSelect == User::invalidNumericalInput)
+		{
+			return;
+		}
+
+		if (aNumberToSelect == 0)
+		{
+			rouletteSelectionBoard[0][0] = aSelectionOwner;
+		}
+
+
+		std::array<int, 2> numberPos = GetIndex(rouletteBoard, aNumberToSelect);
+		const int y = numberPos[0];
+		const int x = numberPos[1];
+		rouletteSelectionBoard[y][x] = aSelectionOwner;
+	}
+
+
+	bool IsPositionOutOfBounds(std::array<int, 2> aGridPosition)
+	{
+		const int y = 0, x = 1;
+		return aGridPosition[y] == User::invalidNumericalInput || aGridPosition[x] == User::invalidNumericalInput;
+	}
+
+	int GetRandomNumberFromRoulette()
+	{
+		int verticalRoll = game::RandomNumber(0, 11);
+		int horizontalRoll = game::RandomNumber(0, 2);
+
+		return rouletteBoard[verticalRoll][horizontalRoll];
 	}
 
 
@@ -138,10 +443,9 @@ namespace Roulette
 			return TryGuessANumber();
 		}
 
-		int verticalRoll = game::RandomNumber(0, 11);
-		int horizontalRoll = game::RandomNumber(0, 2);
 
-		int resultingRoll = roulleteBoard[verticalRoll][horizontalRoll];
+
+		int resultingRoll = GetRandomNumberFromRoulette();
 
 		std::cout << "Number rolled: " << resultingRoll << "\nUser guess: " << userInput << std::endl;
 		system("pause");
@@ -158,18 +462,19 @@ namespace Roulette
 
 	bool TrySelectBetType(BetType& aValidBetType)
 	{
-		std::cout << "How do you want to bet?[Straight|Split|RedOrBlack|OddOrEven|Column]: " << std::endl;
+		std::cout << "How do you want to bet? <[Straight|Split|Corner|RedOrBlack|OddOrEven|Column]>: " << std::endl;
 
 		const std::string straightBet = "straight";
 		const std::string splitBet = "split";
+		const std::string columnBet = "corner";
 		const std::string redOrBlackBet = "redorblack";
 		const std::string oddOrEvenBet = "oddoreven";
-		const std::string columnBet = "column";
+		const std::string cornerBet = "column";
 
 
 		const std::array<std::string, 20> betOptions = {
 			straightBet, splitBet, redOrBlackBet, oddOrEvenBet,
-			columnBet
+			columnBet, cornerBet
 		};
 
 		std::string userInput = User::GetUserInput(betOptions);
@@ -197,6 +502,10 @@ namespace Roulette
 		else if (userInput == columnBet)
 		{
 			aValidBetType = BetType::Column;
+		}
+		else if (userInput == cornerBet)
+		{
+			aValidBetType = BetType::Corner;
 		}
 
 		return true;
--- Roullete.h
+++ Roullete.h
@@ -13,12 +13,31 @@ namespace Roulette
 		RedOrBlack
 	};
 
+	enum class SelectionType
+	{
+		User,
+		Game,
+		LegalOption,
+		None,
+		Invalid = -200
+	};
+
 
 	void PlayGame();
 	extern std::array<std::string, 20> userInstructions;
 	bool TryGuessANumber();
 	bool TryGuessASplit();
+	bool TryGuessACorner();
 	bool TrySelectBetType(BetType &aValidBetType);
-	int GetValidatedNumberThatIsAdjacent(int anOriginPoint, int aNewNumber);
+	int GetValidatedNumberThatIsAdjacent(std::array<std::array<int, 2>, 4> someLegalPositions, int aNewNumber);
 	int GetBetMultiplier();
+	std::array<std::array<int, 2>, 4> GetLegalPositions(int anOriginPoint);
+	int Search(std::array<std::array<int, 3>, 13> aBoard, int anElementToFind);
+	std::array<int, 2> GetIndex(std::array<std::array<int, 3>, 13> aBoard, int anElementToFindIndex);
+	std::array < std::array <int, 2>, 4 > GetLegalNumbers(std::array<int, 2> aGridCoordinate);
+	int GetValueFromPosition(std::array<int, 2> aPosition);
+	bool IsPositionOutOfBounds(std::array<int, 2> aGridPosition);
+	int GetRandomNumberFromRoulette();
+	void DrawBoard();
+	void SelectElementFromBoard(int aNumberToSelect, SelectionType aSelectionOwner);
 }
